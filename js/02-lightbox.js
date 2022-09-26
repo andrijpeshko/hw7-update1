cce65c0086a725e59881b32b1896d7dfbcf091a6
andrijpeshko <gamesap90@gmail.com>
2022-09-26 17:12:47 +0300

    1

---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createImageGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+          <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          />
+         </a>
+        `;
+    }).join('');
+}
+
+const galleryMarkup = createImageGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+    
+
